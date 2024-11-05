7ba43e3802888bd5adb39dcd5ce3928ae028f1e0
Amar Causevic <amar.causevic01@gmail.com>
2024-11-05 23:17:13 +0100

    LUNCHIFY-BE-4: ApiService
    Modding api service class adding optional parameter and many small changes!

--- Lunchify/Utilities/ApiService.swift
+++ Lunchify/Utilities/ApiService.swift
@@ -4,9 +4,10 @@ final class ApiService {
     static let BASE_URL: String = "http://localhost:3000/api/"
     static let shared = ApiService()
     
-    func fetch<T: Codable>(url: URL, type: T.Type, completionHandler: @escaping (T) -> Void, errorHandler: @escaping (String) -> Void) {
-        
-        let task = URLSession.shared.dataTask(with: url) { (data, response, error) in
+    func fetch<T: Codable>(endpoint: String, type: T.Type, completionHandler: @escaping (T) -> Void, errorHandler: @escaping (String) -> Void) {
+        let request: URLRequest = requestBuilder(endpoint: endpoint, httpMethod: nil)
+
+        let task = URLSession.shared.dataTask(with: request) { (data, response, error) in
             guard let data = data, error == nil else {
                 print(error as Any)
                 errorHandler(error?.localizedDescription ?? "Error!")
@@ -27,12 +28,11 @@ final class ApiService {
         task.resume()
     }
     
-    func manipulate<T: Codable>(url: URL, httpMethod: String, params: AnyObject, type: T.Type, completionHandler: @escaping (T) -> Void, errorHandler: @escaping (String) -> Void) {
-        let _: URLRequest = requestBuilder(url: url, httpMethod: httpMethod)
+    func manipulate<T: Codable>(endpoint: String, httpMethod: String, params: AnyObject, type: T.Type, completionHandler: @escaping (T) -> Void, errorHandler: @escaping (String) -> Void) {
+        let request: URLRequest = requestBuilder(endpoint: endpoint, httpMethod: httpMethod)
+        let data = try! JSONSerialization.data(withJSONObject: params, options: [])
         
-        _ = try! JSONSerialization.data(withJSONObject: params, options: [])
-        
-        let task = URLSession.shared.dataTask(with: url) { (data, response, error) in
+        let task = URLSession.shared.uploadTask(with: request, from: data) { (data, response, error) in
             guard let data = data, error == nil else {
                 print(error as Any)
                 errorHandler(error?.localizedDescription ?? "Error!")
@@ -53,9 +53,9 @@ final class ApiService {
         task.resume()
     }
     
-    func requestBuilder(endpoint: String, httpMethod: String) -> URLRequest {
+    func requestBuilder(endpoint: String, httpMethod: String?) -> URLRequest {
         let base = "http://localhost:3000/api/"
-        let url = URL(string: base + endpoint)
+        guard let url = URL(string: base + endpoint) else {fatalError("URL builder has failed")}
         var request = URLRequest(url: url)
         
         request.httpMethod = httpMethod
